346f10470bc9f162c28cc4a92c6ef24da970b2e4
katarina.pavlovic <katarina.pavlovic@united.cloud>
2023-11-14 12:42:02 +0100

    Updated index controller for debugging purposes.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-        return '<h1>Hello! Dolazim iz index kontrolera.</h1>';
+        $url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.3211301&lon=21.8959232&appid=60825efadeb08154a146559d1016ff34';
+        $response = file_get_contents($url);
+        return  "<h3>Ovo dolazi iz index controllera.</h3>" . $response . "<br>";
     }
 }
